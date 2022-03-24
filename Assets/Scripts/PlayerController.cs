cd169a8d5dd0404b4189ec4b42b012db43e79f6f
Trey Werr <treywerr@gmail.com>
2022-03-24 17:50:38 -0500

    Fix player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
 
-    [SerializeField] private float defaultSpeed = 0.1f;
-    [SerializeField] private float fallSpeed = .5f;
+    [SerializeField] private float defaultSpeed = 5f;
+    [SerializeField] private float fallSpeed = 1f;
     private float speed;
     private float horizontalInput;
     private float verticalInput;
@@ -24,10 +24,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get the movement inputs
+        /* Get the movement inputs */
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
+        /* Construct movement vector */
         Vector3 move;
 
         if (!controller.isGrounded)
@@ -43,8 +44,9 @@ public class PlayerController : MonoBehaviour
             speed = defaultSpeed;
 
         if (horizontalInput != 0 || verticalInput != 0)
-            move += new Vector3(horizontalInput, 0, verticalInput) * speed;
+            move += (transform.right * horizontalInput + transform.forward * verticalInput) * speed * Time.deltaTime;
 
+        /* Execute movement */
         controller.Move(move);
     }
 }
