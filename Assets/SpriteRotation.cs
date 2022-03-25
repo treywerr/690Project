6f97fe155ddaf531858cae33d65a66751f8f94a7
treywerr <treywerr@gmail.com>
2022-03-25 11:50:47 -0500

    Implement LookAt for sprites

---
+++ Assets/SpriteRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRotation : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotation.x = 0;
+        rotation.z = 0;
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
